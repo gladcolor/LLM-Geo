e7511255437c08b592097fb8979553c7e62dca33
Huan Ning <gladcolor@gmail.com>
2023-05-18 07:56:30 -0400

    Feature: added Case 3: COVID-19 death rate mapping.

--- LLM_Geo_Constants.py
+++ LLM_Geo_Constants.py
@@ -30,7 +30,7 @@ G.add_edge("load_haz_waste_shp", "haz_waste_gdf")
 """
 graph_requirement = [   
                         'Think step by step.',
-                        # 'DO NOT over-split task into too many small steps, especially for simple problems. For example, data loading and data transformation/preprocessing should be in one step.',
+                        
                         'Steps and data (both input and output) form a graph stored in NetworkX. Disconnected components are NOT allowed.',
                         'Each step is a data process operation: the input can be data paths or variables, and the output can be data paths or variables.',
                         'There are two types of nodes: a) operation node, and b) data node (both input and output data). These nodes are also input nodes for the next operation node.',
@@ -49,6 +49,12 @@ graph_requirement = [
                         'You need spatial data (e.g., vector or raster) to make a map.',
                         'Do not put the GraphML writing process as a step in the graph.',
                          ]
+
+# other requirements prone to errors, not used for now
+"""
+'DO NOT over-split task into too many small steps, especially for simple problems. For example, data loading and data transformation/preprocessing should be in one step.',
+"""
+
 
 
 #--------------- constants for operation generation  ---------------
@@ -74,21 +80,27 @@ operation_requirement = [
                         "You need to receive the data from the functions, DO NOT load in the function if other functions have loaded the data and returned it in advance.",
                         "Note module 'pandas' has no attribute 'StringIO'",
                         "Use the latest Python module methods.",
-                        "When doing spatial analysis, convert the involved layers into the same map projection.",
+                        "When doing spatial analysis, convert the involved spatial layers into the same map projection.",
                         "DO NOT reproject or set spatial data(e.g., GeoPandas Dataframe) if only one layer involved.",
-                        # "Map projection conversion is only conducted for spatial data layers such as GeoDataFrame. DataFrame loaded from a CSV file does not have map projection information.",
-                        "When joining tables, convert the involved columns to string type without leading zeros.",
+                        "Map projection conversion is only conducted for spatial data layers such as GeoDataFrame. DataFrame loaded from a CSV file does not have map projection information.",
+                        "If join DataFrame and GeoDataFrame, using common columns, DO NOT convert DataFrame to GeoDataFrame.",
+                        "When joining tables, convert the involved columns to string type without leading zeros. If joining FIPS or GEOID, need to fill the leading zeros (digits: state: 2, county: 5, tract: 11, block group: 12.",
                         "When doing spatial joins, remove the duplicates in the results. Or please think about whether it needs to be removed.",
-                        "If using colorbar in GeoPandas maps, set the colorbar's height or length as the same as the map.",
+                        "If using colorbar for GeoPandas or Matplotlib visulization, set the colorbar's height or length as the same as the plot.",
                         "Remember the column names and file names used in ancestor functions when joining tables.",
                         "Show a progressbar (e.g., tqdm in Python) if loop more than 10 times, also add exception handling for loops to make sure the loop can run.",
                         "When crawl the webpage context to ChatGPT, using Beautifulsoup to crawl the text only, not all the HTML file.",
                         "If using GeoPandas for spatial joining, the arguements are: geopandas.sjoin(left_df, right_df, how='inner', predicate='intersects', lsuffix='left', rsuffix='right', **kwargs), how: default ‘inner’, use intersection of keys from both dfs; retain only left_df geometry column; ‘left’: use keys from left_df, retain only left_df geometry column. ",
-                        # "Create a copy or use .loc to avoid SettingWithCopyWarning when using pandas DataFrames.",
                         "GEOID in US Census data and FIPS in Census boundaries are integer with leading zeros. If use pandas.read_csv() to GEOID or FIPS columns from read CSV files, set the dtype as 'str'.",
-                        "Drop nan rows when using Pandas, but need to report it.",
+                        "Drop nan rows before using Pandas columns to join, but need to report it.",
+                        # 
                         
                         ]
+# other requirements prone to errors, not used for now
+"""
+"Create a copy or use .loc to avoid SettingWithCopyWarning when using pandas DataFrames."
+"If make maps or graphs, make them looks beautiful and professional, carefuly select color, and show the layout, aspect, size, legend, scale bar, colorbar, background, annotation, axis ticks, title, font size, and label appropriately, but not overloaded."
+"""
 
 
 #--------------- constants for assembly prompt generation  ---------------
