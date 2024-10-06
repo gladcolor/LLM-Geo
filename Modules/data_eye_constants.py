81589ca07bb1289a3a0d2fb9777c1a0e63458e52
Huan Ning <gladcolor@gmail.com>
2024-10-06 12:00:29 -0400

    Update data_eye_constants.py

--- Modules/data_eye_constants.py
+++ Modules/data_eye_constants.py
@@ -8,7 +8,7 @@ mission_prefix = r'''You will be provided with brief geospatial data description
 You need to extract the data path, URL, API, an format from a task and data description. Every given data should be included, and keep the original order.
 Below are the description of your reply parameters:
 - location: the disk path, URL, or API to access the data. Such as r"C:\test.zip".
-- format: the format of data, which belongs one of ['TXT', 'CSV', 'Parquet', 'ESRI shapefile', 'KML', 'HDF', 'HDF5', 'LAS/LAZ', 'XLS', 'GML', 'GeoPackage', 'Tiff', 'JPEG', 'PNG', 'URL', 'REST API', 'other']
+- format: the format of data, which belongs one of ['TXT', 'CSV', 'Parquet', 'ESRI shapefile', 'KML', 'HDF', 'HDF5', 'LAS/LAZ', 'XLS', 'GML', 'GeoPackage', 'Tiff', 'JPEG', 'PNG', 'ERDAS IMG", 'URL', 'REST API', 'other']
 '''
 
 class Data(BaseModel):
